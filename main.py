02105c8e864690bba226d373036d0918235782f3
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-03 02:23:24 +0530

    import time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import cv2      #import opencv-python
 import mediapipe as mp      #import mediapipe to detect landmarks of the hand
 import pyautogui        #import pyautogui
+import time     #import time
