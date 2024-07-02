73a284a50b423f450144ecd596ca91fe88ee6736
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-03 02:23:58 +0530

    implement logic for finger count

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ import cv2      #import opencv-python
 import mediapipe as mp      #import mediapipe to detect landmarks of the hand
 import pyautogui        #import pyautogui
 import time     #import time
+
+#implement logic
+def count_fingers(lst):
+    cnt = 0
+    thresh = (lst.landmark[0].y * 100 - lst.landmark[9].y * 100) / 2
+
+    if (lst.landmark[5].y * 100 - lst.landmark[8].y * 100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[9].y * 100 - lst.landmark[12].y * 100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[13].y * 100 - lst.landmark[16].y * 100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[17].y * 100 - lst.landmark[20].y * 100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[5].x * 100 - lst.landmark[4].x * 100) > 6:
+        cnt += 1
+
+    return cnt
+
